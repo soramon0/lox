e70d676f22d626d554703e24044e18a1dd5f36f1
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-04-10 02:11:02 +0100

    feat: print prompt

--- main.c
+++ main.c
@@ -1,36 +1,47 @@
 #include "src/lox.h"
 
-void run(char *src)
+void	run(char *src)
 {
-  printf("%s", src);
-  free(src);
+	printf("%s", src);
+	free(src);
 }
 
-void runPrompt()
+void	runPrompt(void)
 {
+	char	*line;
+
+	while (1)
+	{
+		line = readline("shell> ");
+		if (line == NULL)
+			break ;
+		run(line);
+	}
 }
 
-void runFile(char *file)
+void	runFile(char *file)
 {
-  char *bytes = read_entire_file(file);
-  if (bytes == NULL)
-  {
-    perror(file);
-    exit(EX_DATAERR);
-  }
-  run(bytes);
+	char	*bytes;
+
+	bytes = read_entire_file(file);
+	if (bytes == NULL)
+	{
+		perror(file);
+		exit(EX_DATAERR);
+	}
+	run(bytes);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-  if (argc > 2)
-  {
-    printf("Usage: jlox [script]\n");
-    exit(EX_USAGE);
-  }
-  else if (argc == 2)
-    runFile(argv[1]);
-  else
-    runPrompt();
-  return (EXIT_SUCCESS);
+	if (argc > 2)
+	{
+		printf("Usage: jlox [script]\n");
+		exit(EX_USAGE);
+	}
+	else if (argc == 2)
+		runFile(argv[1]);
+	else
+		runPrompt();
+	return (EXIT_SUCCESS);
 }
